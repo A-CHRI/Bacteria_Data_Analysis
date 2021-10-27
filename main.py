0fdef4250c8be6fa7c6329dfff138edcd748064d
VeryThankYou <44473602+VeryThankYou@users.noreply.github.com>
2021-10-27 16:47:46 +0200

    Prøvet at arbejde lidt på main, der skal dog stor tid rettes

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import math
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -81,60 +80,70 @@ def dataPlot(data):
     plt.show()
 
 # Main Script
-while True:
-    print("Please choose one of the following options:\n1. Load data\n2. Filter data\n3. Display statistics\n4. Generate plots\n5. Quit")
-    choice = input()
-    if choice == "1":
-        data = input("Please input the filename including .txt (ex. file.txt):")
-        if data[-4] != ".txt":
-            print("File does not end with .txt")
+if __name__ == '__main__':
+    while True:
+        data = np.empty((0,3))
+        choice = input("Please choose one of the following options by entering its corresponding number:\n1. Load data\n2. Filter data\n3. Display statistics\n4. Generate plots\n5. Quit\n")
+        if choice == "1":
+            filename = input("Please input the filename including .txt (ex. file.txt):\n")
+            try:
+                data = dataLoad(filename)
+            except:
+                print("Ineligible filename.")
+                continue
+            data = dataLoad(filename)
+            print("\nData has been loaded into the program.\n")
             continue
-        dataLoad(data)
-        continue
-    if choice == "2":
-        # Filter
-        continue
-    if choice == "3":
-        # Display statistics
-        continue
-    if choice == "4":
-        # Display plots
-        continue
-    if choice == "5":
-        break
+        if choice == "2":
+            # Filter
+            continue
+        if choice == "3":
+            inp = input("Please input the statistic you want to have calculated by entering the corresponding number. Your options are as follows:\n1. Mean Temperature.\n2. Mean Growth rate.\n3. Standard deviation of Temperature.\n4. Standard deviation of Growth rate.\n5. Number of samples.\n6. Mean Growth rate for cold samples.\n7. Mean Growth rate for hot samples.\n")
+            statistics = ["Mean Temperature", "Mean Growth rate", "Std Temperature", "Std Growth rate", "Rows", "Mean Cold Growth rate", "Mean Hot Growth rate"]
+            print(data)
+            try:
+                print(dataStatistics(data, statistics[int(inp)-1]) + " is the computed " + str(statistics[int(inp)-1]) + "\n")
+            except:
+                print("Ineligible input, or lack of loaded data.")
+            continue
+        if choice == "4":
+            dataPlot(data)
+            continue
+        if choice == "5":
+            break
 
 
-# Test scenarios
-print("--- TEST: dataLoad function ---")
-file = "fil.txt"
-data = dataLoad(file)
-for x in data:
-    for y in x:
-        print(y, end = " ")
-    print()
+    # Test scenarios
+    print("--- TEST: dataLoad function ---")
+    file = "fil.txt"
+    data = dataLoad(file)
+    for x in data:
+        for y in x:
+            print(y, end = " ")
+        print()
 
-print("--- TEST: Statistics function ---")
+    print("--- TEST: Statistics function ---")
 
-mt = dataStatistics(data, "Mean Temperature")
-print(f"Mean Temperature: {mt}")
+    mt = dataStatistics(data, "Mean Temperature")
+    print(f"Mean Temperature: {mt}")
 
-mgr = dataStatistics(data, "Mean Growth Rate")
-print(f"Mean Growth Rate: {mgr}")
+    mgr = dataStatistics(data, "Mean Growth Rate")
+    print(f"Mean Growth Rate: {mgr}")
 
-st = dataStatistics(data, "Std Temperature")
-print(f"Std Temperature: {st}")
+    st = dataStatistics(data, "Std Temperature")
+    print(f"Std Temperature: {st}")
 
-sgr = dataStatistics(data, "Std Growth Rate")
-print(f"Std Growth Rate: {sgr}")
+    sgr = dataStatistics(data, "Std Growth Rate")
+    print(f"Std Growth Rate: {sgr}")
 
-rows = dataStatistics(data, "Rows")
-print(f"Rows: {rows}")
+    rows = dataStatistics(data, "Rows")
+    print(f"Rows: {rows}")
 
-mcgr = dataStatistics(data, "Mean Cold Growth Rate")
-print(f"Mean Cold Growth Rate: {mcgr}")
+    mcgr = dataStatistics(data, "Mean Cold Growth Rate")
+    print(f"Mean Cold Growth Rate: {mcgr}")
 
-mhgr = dataStatistics(data, "Mean Hot Growth Rate")
-print(f"Mean Hot Growth Rate: {mhgr}")
+    mhgr = dataStatistics(data, "Mean Hot Growth Rate")
+    print(f"Mean Hot Growth Rate: {mhgr}")
 
-print("--- TEST: dataPlot function ---")
-dataPlot(data)
+    print("--- TEST: dataPlot function ---")
+    dataPlot(data)
