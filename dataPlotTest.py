93488d7b3319234b0b59eccde88eb1b68dd39e8e
Xydros <alexander.anker.chr@gmail.com>
2021-11-10 18:50:18 +0100

    Updated the test scripts

--- dataPlotTest.py
+++ dataPlotTest.py
@@ -2,6 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from dataLoadTest import dataLoad
 
+#This method takes a nx3 matrix of data as parameter, and makes 2 plots of the data, 1 barchart of the bacteria types, 1 graph of the ratio between temperature and growth rate
 def dataPlot(data):
     #Count sample size for each bacteria
     c_1 = np.count_nonzero(data[:, 2] == 1)
@@ -9,35 +10,33 @@ def dataPlot(data):
     c_3 = np.count_nonzero(data[:, 2] == 3)
     c_4 = np.count_nonzero(data[:, 2] == 4)
 
-    #Start subplot for bar-chart
-    bar = plt.subplots()[1]
+    fig, (ax1,ax2) = plt.subplots(1, 2)
+    fig.set_size_inches(12, 6)
+    fig.suptitle("Bacteria data analysis")
 
     #List names of bacteria, and names for x- and y-axis
     names = ["Salmonella\nenterica", "Bascillus cereus", "Listeria", "Brochothrix\nthermosphacta"]
-    bar.set_xlabel("Bacteria Type")
-    bar.set_ylabel("Number of samples")
+    ax1.set_xlabel("Bacteria Type")
+    ax1.set_ylabel("Number of samples")
 
     #Plot and show the bar-chart
-    plt.bar(names, [c_1, c_2, c_3, c_4])
+    ax1.bar(names, [c_1, c_2, c_3, c_4])
 
-    # Start subplot for Growth Rate by Temperature
-    graph = plt.subplots()[1]
-
-    #Set axis sizes
-    plt.axis([10,60,0,1.5])
+    # #Set axis sizes
+    # ax1.axis([10,60,0,1.5])
 
     #Sort the data after temperature
     datasort = data[np.argsort(data[:, 0])]
 
     #Plot each type of bacteria with points and graph, add color-label
     for i in range(4):
-        plt.plot(datasort[np.where(datasort[:, 2] == i+1)][:, 0], datasort[np.where(datasort[:, 2] == i+1)][:, 1], marker='o', label=str(names[i]))
+        ax2.plot(datasort[np.where(datasort[:, 2] == i+1)][:, 0], datasort[np.where(datasort[:, 2] == i+1)][:, 1], marker='o', label=str(names[i]))
 
     #Show color-labels, set names for x- and y-axis, and plot/show the graph
-    plt.legend()
-    graph.set_xlabel("Temperature")
-    graph.set_ylabel("Growth Rate")
-    plt.show()
+    ax2.legend()
+    ax2.set_xlabel("Temperature")
+    ax2.set_ylabel("Growth Rate")
+    fig.show()
 
 
 data = dataLoad("fil.txt")
